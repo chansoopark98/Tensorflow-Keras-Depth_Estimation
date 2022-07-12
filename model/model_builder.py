7dffc5e0d47f380de108124dbbfd3f2bc972c42c
park-tsp <chansoo0710@gmail.com>
2022-07-12 15:37:13 +0900

    Add : dense depth

--- model/model_builder.py
+++ model/model_builder.py
@@ -1,7 +1,11 @@
 import tensorflow
 from .model_zoo.unet import Unet
+from .model_zoo.dense_depth import DepthEstimate
+
 
 def base_model(image_size, output_channel=1):
-    model = Unet(image_size=image_size, input_channel=3, output_channel=output_channel)
     
-    return model.build_generator()
+    # model = Unet(image_size=image_size, input_channel=3, output_channel=output_channel)
+    # model = model.build_generator()
+    model = DepthEstimate()
+    return model
