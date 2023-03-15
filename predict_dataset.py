f1ec2376198c50d7111f083d8bd66c6fde8570e1
Rin98 <chansoo0710@gmail.com>
2023-03-15 15:41:01 +0900

    Test

--- predict_dataset.py
+++ predict_dataset.py
@@ -20,7 +20,7 @@ parser.add_argument("--threshold",           type=float,  help="Post processing 
 parser.add_argument("--checkpoint_dir",      type=str,    help="Setting the model storage directory",
                     default='./checkpoints/')
 parser.add_argument("--weight_name",         type=str,    help="Saved model weights directory",
-                    default='0315/_Bs-32_Ep-50_Lr-0.0004_ImSize-480_Opt-adam_multi-gpu_0315_230314_Res50Depth_customLoss_480x640_adam_lossFactor_best_ssim.h5')
+                    default='0315/_Bs-32_Ep-50_Lr-0.0004_ImSize-480_Opt-adam_multi-gpu_0315_230314_Res50Depth_customLoss_480x640_adam_lossFactor_TransposeConv_best_loss.h5')
 
 args = parser.parse_args()
 
