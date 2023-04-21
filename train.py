c91eaabff261793ccefffea554502c70c79e69cb
chansoo Park <chansoo0710@gmail.com>
2023-04-21 14:47:50 +0900

    Test: Tensorflow 2.12 with RTX4090 Cuda 11.8

--- train.py
+++ train.py
@@ -13,9 +13,9 @@ parser = argparse.ArgumentParser()
 
 # Set Training Options
 parser.add_argument("--model_prefix",     type=str,    help="Model name (logging weights name and tensorboard)",
-                    default='0323_resnet50_0to1_newAug')
+                    default='test_4090')
 parser.add_argument("--batch_size",       type=int,    help="Batch size per each GPU",
-                    default=8)
+                    default=32)
 parser.add_argument("--epoch",            type=int,    help="Training epochs",
                     default=30)
 parser.add_argument("--lr",               type=float,  help="Initial learning rate",
